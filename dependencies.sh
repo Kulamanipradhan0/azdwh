fb431caffc93d6d9f6b2f433015f73434c1e31bf
Kulamanipradhan0 <kulamanipradhan0@gmail.com>
2021-08-08 15:18:46 +0200

    new changes

--- dependencies.sh
+++ dependencies.sh
@@ -19,9 +19,9 @@ mkdir -p Amazon_DWH/log_files/staging
 mkdir -p Amazon_DWH/log_files/dwh
 mkdir -p Amazon_DWH/log_files/temp
 
-mkdir -p Amazon_DWH/archive
+mkdir -p Amazon_DWH/archive/logs
 
-cd Amazon_DWH/archive
+cd Amazon_DWH/archive/
 wget https://github.com/Kulamanipradhan0/azdwh/archive/refs/heads/main.zip
 unzip main.zip 
 
@@ -29,21 +29,25 @@ mv azdwh-main/azdwh_etl_repo ../
 
 cd ../
 current_dir=`pwd`
-# echo "log_file_dir=${current_dir}/log_files" >> ~/.kettle/kettle.properties
-# echo "stg_file_dir=${current_dir}/source_files" >> ~/.kettle/kettle.properties
-# echo "temp_file_location=${current_dir}/log_files" >> ~/.kettle/kettle.properties
+echo "log_file_dir=${current_dir}/log_files" >> ~/.kettle/kettle.properties
+echo "stg_file_dir=${current_dir}/source_files" >> ~/.kettle/kettle.properties
+echo "temp_file_location=${current_dir}/log_files" >> ~/.kettle/kettle.properties
 
 mv ~/.kettle/repositories.xml ~/.kettle/repositories_backup.xml
 echo '<?xml version="1.0" encoding="UTF-8"?>' > ~/.kettle/repositories.xml
 echo "<repositories>  <repository>    <id>KettleFileRepository</id>    <name>azdwh</name>    <description>azdwh</description>    <is_default>true</is_default>" >> ~/.kettle/repositories.xml
 echo "<base_directory>${current_dir}/azdwh_etl_repo</base_directory>    <read_only>N</read_only>    <hides_hidden_files>N</hides_hidden_files>  </repository>  </repositories>" >> ~/.kettle/repositories.xml
 
-mkdir -p tools
-cd tools
+# mkdir -p tools
+# cd tools
 #wget https://sourceforge.net/projects/pentaho/files/latest/download
 #unzip pdi-ce-9.2.0.0-290.zip
 
+psql -h localhost -p 5432 -U postgres -f archive/azdwh-main/db_scripts/admin_create_azdwh_database.sql > archive/logs/admin_create_azdwh_database.log
 
+echo "localhost:5432:*:azdwh_usr:azdwhusr123" >> ~/.pgpass
+
+psql -h localhost -p 5432 -U azdwh_usr azdwh -f archive/azdwh-main/db_scripts/azdwh_usr_create_azdwh_database_objects.sql > archive/logs/azdwh_usr_create_azdwh_database_objects.log
 
 
 
