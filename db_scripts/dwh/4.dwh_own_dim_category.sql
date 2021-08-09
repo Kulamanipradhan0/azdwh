9461106e7655b97815a3293e5626fab12b09d1ee
Kulamanipradhan0 <kulamanipradhan0@gmail.com>
2021-08-09 19:21:07 +0200

    price bucket changes added

--- db_scripts/dwh/4.dwh_own_dim_category.sql
+++ db_scripts/dwh/4.dwh_own_dim_category.sql
@@ -8,8 +8,8 @@ batch_identifier integer not null,
 insert_datetime timestamp without time zone default current_timestamp,
 last_update_datetime timestamp without time zone default current_timestamp,
 category_name character varying(100),
-price_bucket_id integer REFERENCEs dwh_own.dim_price_bucket,
+price_bucket_key integer REFERENCEs dwh_own.dim_price_bucket,
 CONSTRAINT dim_category_pkey PRIMARY KEY (category_key)
 );
 
-create index dim_category_ix_01 on dwh_own.dim_category (price_bucket_id,category_name);
+create index dim_category_ix_01 on dwh_own.dim_category (price_bucket_key,category_name);
--- db_scripts/dwh/4.dwh_own_dim_price_bucket.sql
+++ db_scripts/dwh/4.dwh_own_dim_price_bucket.sql
@@ -1,4 +1,4 @@
-drop table if exists dwh_own.dim_category CASCADE;
+﻿drop table if exists dwh_own.dim_category CASCADE;
 DROP MATERIALIZED VIEW  IF EXISTS dwh_own.mview_review_olap CASCADE;
 drop table if exists dwh_own.dim_price_bucket ;
 
@@ -6,9 +6,12 @@ create table dwh_own.dim_price_bucket(
 price_bucket_key	serial not null,
 batch_identifier integer not null,
 insert_datetime timestamp without time zone default To_char(CURRENT_TIMESTAMP, 'yyyy-mm-dd hh:mi:ss.ms') ::timestamp not null,
-price_bucket_id integer not null,
-description character varying(50) not null,
 reference_price numeric,
+first_interval integer,
+second_interval integer,
+third_interval integer,
+fourth_interval integer,
 CONSTRAINT dim_price_bucket_pkey PRIMARY KEY (price_bucket_key)
 );
-create index dim_price_bucket_ix_01 on dwh_own.dim_price_bucket (price_bucket_id);
+create index dim_price_bucket_ix_01 on dwh_own.dim_price_bucket (price_bucket_key);
+
