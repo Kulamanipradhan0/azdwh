62e519def7e1e42b6bd1b1ff3f5bb6ce27d0feb9
Kulamanipradhan0 <kulamanipradhan0@gmail.com>
2021-08-09 19:55:18 +0200

    new mac file

--- dependencies_mac.sh
+++ dependencies_mac.sh
@@ -1,8 +1,17 @@
+#Command Line arguments
+
+PG_PORT=$1
+
+if [ -z $1 ]; then
+        echo "Please provide Postgres Port using [dependecies.sh <Port Number>] format"
+        exit 1
+fi
+
 #Dependency Library + Directory Structure :
-sudo brew install gzip
-sudo brew install split
-sudo brew install unzip
-sudo brew install wget
+brew install gzip
+brew install split
+brew install unzip
+brew install wget
 
 sudo mkdir -p /var/lib/postgresql/azdwh_data
 sudo mkdir -p /var/lib/postgresql/azdwh_data_ix
@@ -34,27 +43,32 @@ current_dir=`pwd`
 echo "log_file_dir=${current_dir}/log_files" >> ~/.kettle/kettle.properties
 echo "stg_file_dir=${current_dir}/source_files" >> ~/.kettle/kettle.properties
 echo "temp_file_location=${current_dir}/log_files" >> ~/.kettle/kettle.properties
+echo "AZDWH_HOSTNAME=localhost" >> ~/.kettle/kettle.properties
+echo "AZDWH_PORT=5433" >> ~/.kettle/kettle.properties
+echo "AZDWH_DBNAME=azdwh" >> ~/.kettle/kettle.properties
+echo "AZDWH_USERNAME=azdwh_usr" >> ~/.kettle/kettle.properties
+echo "AZDWH_PASSWORD=azdwhusr123" >> ~/.kettle/kettle.properties
+
 
 mv ~/.kettle/repositories.xml ~/.kettle/repositories_backup.xml
 echo '<?xml version="1.0" encoding="UTF-8"?>' > ~/.kettle/repositories.xml
 echo "<repositories>  <repository>    <id>KettleFileRepository</id>    <name>azdwh</name>    <description>azdwh</description>    <is_default>true</is_default>" >> ~/.kettle/repositories.xml
 echo "<base_directory>${current_dir}/azdwh_etl_repo</base_directory>    <read_only>N</read_only>    <hides_hidden_files>N</hides_hidden_files>  </repository>  </repositories>" >> ~/.kettle/repositories.xml
 
+
+
 # mkdir -p tools
 # cd tools
 #wget https://sourceforge.net/projects/pentaho/files/latest/download
 #unzip pdi-ce-9.2.0.0-290.zip
-pwd
-
-# psql -h localhost -p 5432 -U postgres -f archive/azdwh-main/db_scripts/admin_create_azdwh_database.sql > archive/logs/admin_create_azdwh_database.log
-
-# echo "localhost:5432:*:azdwh_usr:azdwhusr123" >> ~/.pgpass
-
-# psql -h localhost -p 5432 -U azdwh_usr azdwh -f archive/azdwh-main/db_scripts/azdwh_usr_create_azdwh_database_objects.sql > archive/logs/azdwh_usr_create_azdwh_database_objects.log
 
 
+cd ${current_dir}/archive/azdwh-main/db_scripts
+psql -h localhost -p ${PG_PORT} -U postgres -f ${current_dir}/archive/azdwh-main/db_scripts/admin_create_azdwh_database.sql > ${current_dir}/archive/logs/admin_create_azdwh_database.log
 
+echo "localhost:${PG_PORT}:*:azdwh_usr:azdwhusr123" >> ~/.pgpass
 
+psql -h localhost -p ${PG_PORT} -U azdwh_usr azdwh -f ${current_dir}/archive/azdwh-main/db_scripts/azdwh_usr_create_azdwh_database_objects.sql > ${current_dir}/archive/logs/azdwh_usr_create_azdwh_database_objects.log
 
 
 
